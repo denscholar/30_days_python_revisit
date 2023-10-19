96be3a1f1ce2e466d16bd26ad411732fe696f5a9
Dennis Akagha <d.akagha20@gmail.com>
2023-10-19 23:18:12 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+name = 'peter'
+print(name)
+
+# expression vs statement
+"""
+expression is just a string but a sstatement perform an operation. 
+"""
+
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*      WELCOME     *"
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
